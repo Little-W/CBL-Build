f9e81ace7ca75166857dfb86c7f46f7ebb0651f3
Little-W <1405481963@qq.com>
2021-07-28 10:03:02 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ set -eu
 function parse_parameters() {
     while ((${#})); do
         case ${1} in
-            all | binutils | deps | kernel | llvm) ACTION=${1} ;;
+            all | deps | build | upload  ) ACTION=${1} ;;
             *) exit 33 ;;
         esac
         shift
@@ -16,19 +16,14 @@ function parse_parameters() {
 
 function do_all() {
     do_deps
-    do_llvm
-    do_binutils
-    do_kernel
-}
-
-function do_binutils() {
-    "${BASE}"/build-binutils.py -t x86_64
+    do_upload
+    do_build
 }
 
 function do_deps() {
     # We only run this when running on GitHub Actions
-    [[ -z ${GITHUB_ACTIONS:-} ]] && return 0
-    sudo apt-get install -y --no-install-recommends \
+    sudo apt update
+    sudo apt install -y --no-install-recommends \
         bc \
         bison \
         ca-certificates \
@@ -49,26 +44,69 @@ function do_deps() {
         texinfo \
         xz-utils \
         zlib1g-dev
+    git config --global user.email "1405481963@qq.com"
+    git config --global user.name "Little-W"
+	sudo dd if=/dev/zero of=/swapfile bs=1M count=22960
+	sudo mkswap /swapfile
+	sudo swapon /swapfile
+}
+function do_build() {
+    ./build-llvm.py \
+		--clang-vendor "Sakura-🌸" \
+		--targets "ARM;AArch64;X86" \
+		--lto thin \
+		--incremental \
+		--pgo kernel-defconfig
+     #	--build-stage1-only \
+    #	--install-stage1-only \
+     #	--projects "clang;compiler-rt;lld;polly" \
+    	#--branch "release/12.x" 
+    ./build-binutils.py --targets arm aarch64 x86_64 
+# Remove unused products
+
+rm -f install/lib/*.a install/lib/*.la install/lib/clang/*/lib/linux/*.a*
+
+for f in $(find install -type f -exec file {} \;); do
+	if [ -n "$(echo $f | grep 'ELF .* interpreter')" ]; then
+		i=$(echo $f | awk '{print $1}'); i=${i: : -1}
+		# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+		if [ -d $(dirname $i)/../lib/ldscripts ]; then
+			patchelf --set-rpath '$ORIGIN/../../lib:$ORIGIN/../lib' "$i"
+		else
+			if [ "$(patchelf --print-rpath $i)" != "\$ORIGIN/../../lib:\$ORIGIN/../lib" ]; then
+				patchelf --set-rpath '$ORIGIN/../lib' "$i"
+			fi
+		fi
+		# Strip remaining products
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			${stripBin} --strip-unneeded "$i"
+		fi
+	elif [ -n "$(echo $f | grep 'ELF .* relocatable')" ]; then
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			i=$(echo $f | awk '{print $1}');
+			${stripBin} --strip-unneeded "${i: : -1}"
+		fi
+	else
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			i=$(echo $f | awk '{print $1}');
+			${stripBin} --strip-all "${i: : -1}"
+		fi
+	fi
+done
+
 }
 
-function do_kernel() {
-    cd "${BASE}"/kernel
-    ./build.sh -t X86
-}
-
-function do_llvm() {
-    EXTRA_ARGS=()
-    [[ -n ${GITHUB_ACTIONS:-} ]] && EXTRA_ARGS+=(--no-ccache)
-    "${BASE}"/build-llvm.py \
-        --assertions \
-        --branch "release/12.x" \
-        --build-stage1-only \
-        --check-targets clang lld llvm \
-        --install-stage1-only \
-        --projects "clang;lld" \
-        --shallow-clone \
-        --targets X86 \
-        "${EXTRA_ARGS[@]}"
+function do_upload() {
+    rel_date="$(date "+%Y%m%d")" # ISO 8601 format
+    clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
+    # Generate build info
+    git clone --depth 1 git@github.com:Little-W/Sakura-CBL.git ~/cl/
+    mv ~/cl/.git install/.git
+    mv ~/cl/README.md install/README.md
+    cd  install
+    git add -A -f
+    git commit -am "Update to $rel_date build (Clang Version: $clang_version)"
+    git push
 }
 
 parse_parameters "${@}"
