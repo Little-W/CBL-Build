392e2f0479b9e94986ed253c545da1c05f4070cb
Yusen <1405481963@qq.com>
2025-08-15 16:25:04 +0800

    disable lto

--- build.sh
+++ build.sh
@@ -17,7 +17,6 @@ set -eu
 # 执行构建 LLVM 工具链的命令，并启用 PGO (Profile-Guided Optimization)
 "$base"/build-llvm.py \
     --vendor-string "Sakura-🌸-RISCV" \
-    --lto thin \
     --build-target distribution \
     --install-folder "$install" \
     --install-target distribution \
